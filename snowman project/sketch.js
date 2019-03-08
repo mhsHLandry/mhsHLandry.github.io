1957289a9c1b716fec09311d8aa95ea8eb28d744
mhsHLandry <48356548+mhsHLandry@users.noreply.github.com>
2019-03-08 08:59:32 -0500

    Add files via upload

---
+++ snowman project/sketch.js
@@ -0,0 +1,19 @@
+// Hunter Landry - 3/7/2019
+function setup() {
+  // put setup code here
+  createCanvas(400,400)
+}
+
+function draw() {
+  //floor
+  rect(0,300,400,110);
+  //bottom
+ellipse(200, 300, 150, 150);
+//middle
+ellipse(200, 200, 100, 100);
+//head
+ellipse(200, 120, 75, 75);
+// arms
+line(160,200,112,114);
+line(240,200,300,155);
+}
